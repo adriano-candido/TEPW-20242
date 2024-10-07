e617b68d5f3470c3711fe97f887eb396def4977a
Adriano Lima <jink_adriano@hotmail.com>
2024-10-07 20:55:20 -0300

    aula 4/10

--- backend/src/main/java/br/edu/unichristus/BackendApplication.java
+++ backend/src/main/java/br/edu/unichristus/BackendApplication.java
@@ -2,6 +2,21 @@ package br.edu.unichristus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API Backend Topicos Especiais",
+				version = "1.0",
+				description = "Trata-se da API que está sendo desenvolvida na disicplina TEPW",
+				contact = @Contact(
+						name = "Unichristus",
+						email = "adriano.candido@unichristus.edu.br",
+						url = "https://www.google.com")
+		)
+)
 
 @SpringBootApplication
 public class BackendApplication {
