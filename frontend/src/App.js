a1082eb280278a646bf1eeaf33695add5e0d9e6a
Adriano Lima <jink_adriano@hotmail.com>
2024-11-18 21:49:19 -0300

    aula 18-11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import Header from './Header';
+import './global.css';
+
+import Login from './pages/Login'
 
 export default function App() {
-  let counter=0;
-
-  function increment(){
-    counter++;
-    console.log(counter);
-  }
-
   return (
-    <div>
-    <Header>
-      Titulo passado via children (Propriedade)
-      <h1>Contador: {counter}</h1>
-    </Header>
-    <button onClick={increment}>Add</button>
-    </div>
+   <Login />
 
   );
 }
+ 
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+import logoImage from '../../assets/logo.png';
+import padlock from '../../assets/padlock.png';
+
+export default function Login() {
+  return (
+   <h1>BEM VINDO</h1>
+
+  );
+}
