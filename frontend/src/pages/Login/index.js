e0b88129b5bbefbface3b88b2e6974e0bd8152a3
Adriano Lima <jink_adriano@hotmail.com>
2024-11-22 21:13:22 -0300

    aula 22-11

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -6,7 +6,18 @@ import padlock from '../../assets/padlock.png';
 
 export default function Login() {
   return (
-   <h1>BEM VINDO</h1>
+   <div className="login-container">
+      <section className="form">
+        <img src={logoImage} alt="Imagem Logo Unichristus" />
+        <form>
+          <h1>Acesse sua conta</h1>
+          <input placeholder="Usuário"/>
+          <input type="password" placeholder="Senha"/>
+          <button className="button" type="submit">Entrar</button>
+        </form>
+      </section>
+      <img src={padlock} alt="Imagem Cadeado" />
+   </div>
 
   );
 }
