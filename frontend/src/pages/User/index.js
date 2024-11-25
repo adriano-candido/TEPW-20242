50127fa92c67293e739af5a0c29ec90e22ec622b
Adriano Lima <jink_adriano@hotmail.com>
2024-11-25 20:39:15 -0300

    aula 22-11

--- frontend/src/pages/User/index.js
+++ frontend/src/pages/User/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPower, FiEdit, FiTrash2} from 'react-icons/fi';
+
 import './styles.css';
 
 import logoImage from '../../assets/logo.png';
