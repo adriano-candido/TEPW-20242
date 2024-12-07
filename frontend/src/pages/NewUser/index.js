dd02f1d1ecd6f2662be17586dc2f337613af8e20
Adriano Lima <jink_adriano@hotmail.com>
2024-12-06 21:03:28 -0300

    aula 06-12

--- frontend/src/pages/NewUser/index.js
+++ frontend/src/pages/NewUser/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './style.css';
 import logoImage from '../../assets/logo.png';
 
 export default function NewUser(){
+
+    const [id, setId] = useState(null);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return(
         <div className="new-user-container">
             <div className="content">
@@ -19,10 +29,23 @@ export default function NewUser(){
                     </Link>
                 </section>
                 <form>
-                    <input placeholder="Nome Completo" />
-                    <input placeholder="E-mail" />
-                    <input placeholder="Login" />
-                    <input placeholder="Password" />
+                    <input 
+                    value={name}
+                    onChange={ e => setName(e.target.value)}
+                    placeholder="Nome Completo" 
+                    />
+                    <input 
+                    value={email}
+                    onChange={ e => setEmail(e.target.value)}
+                    placeholder="E-mail" />
+                    <input 
+                    value={login}
+                    onChange={ e => setLogin(e.target.value)}
+                    placeholder="Login" />
+                    <input 
+                    value={password}
+                    onChange={ e => setPassword(e.target.value)}
+                    placeholder="Password" />
 
                     <button type="submit" className="button">
                         Salvar
