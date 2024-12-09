eeb850eff11607301b24cf9cd2c59fcd257200cc
Adriano Lima <jink_adriano@hotmail.com>
2024-12-09 20:32:12 -0300

    aula 6-12

--- frontend/src/pages/Users/index.js
+++ frontend/src/pages/Users/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import {FiPower, FiEdit, FiTrash2} from 'react-icons/fi';
 
-import './style.css';
+import api from '../../services/api';
 
+import './style.css';
 import logoImage from '../../assets/logo.png';
 
 export default function Users(){
+
+    const [users, setUsers] = useState([]);
+
+    const name = localStorage.getItem('name');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        api.get("v1/user/all").then(response =>{
+            setUsers(response.data);
+        })
+    }, [])
     return (
        <div className="user-container">
             <header>
                 <img src={logoImage} alt="Imagem Logo Unichristus" />
-                <span>Bem vindo, <strong>Usuário</strong></span>
+                <span>Bem vindo, <strong>{name}</strong></span>
                 <Link className="button" to="/user/new">
                 Adicionar Usuário
                 </Link>
@@ -22,15 +35,16 @@ export default function Users(){
 
             <h1>Lista de Usuários</h1>
             <ul>
-                <li>
+                {users.map(user =>
+                    <li>
                     <strong>Código</strong>
-                    <p>1</p>
+                    <p>{user.id}</p>
                     <strong>Nome Completo</strong>
-                    <p>Joao da Cruz</p>
+                    <p>{user.name}</p>
                     <strong>E-mail</strong>
-                    <p>joao@gmail.com</p>
+                    <p>{user.email}</p>
                     <strong>Usuário</strong>
-                    <p>jao</p>
+                    <p>{user.login}</p>
 
                     <button type="button">
                         <FiEdit size={20} color="#251FC5" />
@@ -39,57 +53,8 @@ export default function Users(){
                         <FiTrash2 size={20} color="#251FC5" />
                     </button>
                 </li>
-                <li>
-                    <strong>Código</strong>
-                    <p>1</p>
-                    <strong>Nome Completo</strong>
-                    <p>Joao da Cruz</p>
-                    <strong>E-mail</strong>
-                    <p>joao@gmail.com</p>
-                    <strong>Usuário</strong>
-                    <p>jao</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-                </li>
-                <li>
-                    <strong>Código</strong>
-                    <p>1</p>
-                    <strong>Nome Completo</strong>
-                    <p>Joao da Cruz</p>
-                    <strong>E-mail</strong>
-                    <p>joao@gmail.com</p>
-                    <strong>Usuário</strong>
-                    <p>jao</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-                </li>
-                <li>
-                    <strong>Código</strong>
-                    <p>1</p>
-                    <strong>Nome Completo</strong>
-                    <p>Joao da Cruz</p>
-                    <strong>E-mail</strong>
-                    <p>joao@gmail.com</p>
-                    <strong>Usuário</strong>
-                    <p>jao</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-                </li>
+                )}
+              
             </ul>
 
        </div>
